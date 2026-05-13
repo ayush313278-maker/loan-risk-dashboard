c8d5f901bad55f10dd23505c6f939fa7b80019e6
ayushkumar032024-sys <ayushkumar032024@gmail.com>
2026-05-14 02:11:06 +0530

    Loan Portfolio Dashboard

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+customers = pd.read_csv("../loan_customers_data.csv")
+
+st.title("Loan Portfolio Risk Dashboard")
+
+st.write(customers.head())
+
+fig, ax = plt.subplots()
+
+sns.countplot(x="default_flag", data=customers, ax=ax)
+
+st.pyplot(fig)
+
+st.subheader("Dataset Shape")
+st.write(customers.shape)
+
+st.subheader("Credit Score Distribution")
+fig2, ax2 = plt.subplots()
+sns.histplot(customers["credit_score"], bins=20)
+st.pyplot(fig2)
+
+st.subheader("Future Risk Analysis")
+
+avg_credit = customers["credit_score"].mean()
+
+if avg_credit < 500:
+    st.error("High Risk Portfolio")
+else:
+    st.success("Moderate Risk Portfolio")
